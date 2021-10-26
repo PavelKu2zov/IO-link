31f7cb1ff8c19d98537978dfe8aca3d68ad4f8c3
Kutuzov Pavel <ku2zovpavel@gmail.com>
2021-10-26 23:25:14 +0300

    Added some declarations of Rtos variables

--- source/main.c
+++ source/main.c
@@ -7,84 +7,19 @@
 
 
 #include "Init.h"
-#include <stdio.h>
-
-#define FORWARD		0
-#define BACKWARD	1
-
-#define NOREADY		0
-#define READY		1
-#define INIT		3
 
 
-volatile uint8_t encoder_status = INIT;
-volatile uint8_t encoder_direction = FORWARD;
-char buffer[80];
-void USARTSend(const unsigned char *pucBuffer, uint8_t size);
+xSemaphoreHandle xSemaphorePL;
+
+
 int main(void)
 {
-Init();
+	Init();
 
-while(1)
-{
-  if (encoder_status)
-    {
-       encoder_status = NOREADY;
-       uint8_t nSize=0;
-       if (encoder_direction == FORWARD)
-       {
-          //nSize = sprintf(buffer, "FORWARD\r\n");  
-          buffer[0] = 0xb4;
-          buffer[1] = 0x01;
-          buffer[2] = 0x01; 
-
-          nSize = 3;
-       }
-        else
-        {
-          //nSize = sprintf(buffer, "BACKWARD\r\n");
-          buffer[0] = 0xb4;
-          buffer[1] = 0x01;
-          buffer[2] = 0x7f;
-          nSize = 3;
-               
-        }
-        USARTSend(buffer,nSize);
-       
-       
-     
-    }
-}
+// Create semaphore for PL service
+	vSemaphoreCreateBinary( xSemaphorePL );	
 
 }
 
 
 
-void TIM2_IRQHandler(void)
-{
-	if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET)
-	{
-		TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
-
-		if (encoder_status == INIT)
-			encoder_status = NOREADY;
-		else
-			encoder_status = READY;
-
-		
-		encoder_direction = (TIM2->CR1 & TIM_CR1_DIR ? BACKWARD : FORWARD);
-	}
-}
-
-
-void USARTSend(const unsigned char *pucBuffer, uint8_t size)
-{
-    while (0 != size)
-    {
-        USART_SendData(USART2, *pucBuffer++);
-        while(USART_GetFlagStatus(USART2, USART_FLAG_TC) == RESET)
-        {
-        }
-        size--;
-    }
-}
